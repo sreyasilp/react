9ca952d5e2c30e19c8f4004aec828150d0d8b864
sreyasilp <sreyas.tp@experionglobal.com>
2023-04-09 08:08:38 +0530

    footer alignment fix

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -5,7 +5,7 @@ export const Footer = () => {
         <div className='footer-content position-relative'>
           <div className='container'>
             <div className='row'>
-              <div className='col-lg-4 col-md-6'>
+              <div className='col-lg-9 col-md-6'>
                 <div className='footer-info'>
                   <h3>Paddy View Resort</h3>
                   <p>
@@ -47,7 +47,7 @@ export const Footer = () => {
                 </div>
               </div>
               {/* End footer info column*/}
-              <div className='col-lg-8 col-md-3 footer-links'>
+              <div className='col-lg-2 col-md-3 footer-links'>
                 <h4>Useful Links</h4>
                 <ul>
                   <li>

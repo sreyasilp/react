631bd847f3f40ab0ba0b0c7c41fb57792dfcb5c5
sreyasilp <sreyas.tp@experionglobal.com>
2023-04-19 06:24:56 +0530

    add video

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -4,10 +4,12 @@ import { Construction } from './facilitiesSection';
 import { Features } from './featuresSection';
 import { Project } from './gallerySection';
 import { Services } from './servicesSection';
+import { Videos } from './videoSection';
 
 export const Main = () => {
   return (
     <main id='main'>
+      <Videos />
       <Construction />
       <Services />
       <AlternateServices />
---
+++ src/pages/Main/videoSection.js
@@ -0,0 +1,27 @@
+export const Videos = () => {
+  return (
+    <section id='constructions' className='constructions'>
+      <div className='container' data-aos='fade-up'>
+        <div className='section-header'>
+          <h2>Videos</h2>
+          <p>
+          Take a look at this video to know more about Paddy View Resort
+          </p>
+        </div>
+        <div className='row gy-12'>
+          <div className='col-lg-12' data-aos='fade-up' data-aos-delay={100}>
+            <div className='card-item'>
+              <div className='row'>
+                <div className='col-xl-12 d-flex align-items-center'>
+                  <div className='card-body'>
+                  <iframe width="1120" height="630" src="https://www.youtube.com/embed/txSpq5ftkFo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen" allowfullscreen></iframe>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
